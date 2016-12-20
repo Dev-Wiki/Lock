cca789678d701e19ea14f2d20857f04325cc92da
DevWiki <674952098@qq.com>
2016-12-20 20:48:29 +0800

    添加apk文件和英文翻译

--- app/src/main/java/net/devwiki/lock/MainActivity.java
+++ app/src/main/java/net/devwiki/lock/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends Activity {
 
